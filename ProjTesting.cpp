d11f8d1896771b5245437b21eee0441d84212f39
Goncharova Yulia <gontch.iulia@gmail.com>
2021-12-23 08:33:40 +0300

    add mercator variant B

--- ProjTesting.cpp
+++ ProjTesting.cpp
@@ -3,7 +3,8 @@
 #include <iomanip>
 
 #include "BritishNationalGrid.h"
-#include "MercatorVariantD.h"
+#include "MercatorVariantB.h"
+#include "Amersfoort.h"
 
 bool CalcProj(
     const char* src, //"EPSG:4326"
@@ -14,9 +15,21 @@ bool CalcProj(
     double& out_second
 )
 {
+	/*
+	PJ *proj_create_conversion_lambert_conic_conformal_2sp_michigan(
+    PJ_CONTEXT *ctx, double latitude_false_origin,
+    double longitude_false_origin, double latitude_first_parallel,
+    double latitude_second_parallel, double easting_false_origin,
+    double northing_false_origin, double ellipsoid_scaling_factor,
+    const char *ang_unit_name, double ang_unit_conv_factor,
+    const char *linear_unit_name, double linear_unit_conv_factor)
+	*/
+
+
     PJ* P;
     PJ_COORD c, c_out;
 
+	
     P = proj_create_crs_to_crs(PJ_DEFAULT_CTX, src, trg, NULL);
 
     if (P == 0)
@@ -43,6 +56,7 @@ int main()
     std::cout << "Our Proj Tests" << std::endl;
     std::cout << "-----------------------------------------" << std::endl;
     BritishNationalGrid();
-    MercatorVariantD();
+    MercatorVariantB();
+	Amersfoort();
     return 0;
 }
