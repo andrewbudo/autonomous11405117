4df3bf2f4c5c837baf292fa4737b6a8268825046
Andrew Budo <47389604+andrewbudo@users.noreply.github.com>
2021-12-23 10:22:04 +0300

    Update Amersfoort.cpp

--- Amersfoort.cpp
+++ Amersfoort.cpp
@@ -11,7 +11,7 @@ void Amersfoort()
 	double lon = 6;
 	std::cout << std::fixed << "lat = " << lat << "; lon = " << lon << "\n";
 	const char* src = "+proj=longlat +datum=WGS84";
-	const char* trg = "+proj=tmerc +lon_0=5.387639 +lat_0=52.156161 +a=6377397.155 +rf=299.15281 +x_o=155000 +y_0=463000 +k_0=0.9999079 +e=0.08169683 +units=m";
+	const char* trg = "+proj=sterea +lat_0=52.156161 +lon_0=5.387639  +k_0=0.9999079 +x_0=155000  +y_0=463000.0 +a=6377397.155 +rf=299.15281 +units=m";
 
 	double out_N, out_E;
 	if (CalcProj(src, trg, lat, lon, out_N, out_E))
